9d0e681b778ae3abc857953d2380aa60d375925c
Michael <michael.gamel@hotmail.com>
2019-08-20 00:30:13 +0400

    Channel Metadata Box

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,74 @@
 import React, { Component } from 'react';
+import { Segment, Accordion, Header, Icon, Image } from 'semantic-ui-react';
 
 class MetaPanel extends Component {
+  state = {
+    channel: this.props.currentChannel,
+    privateChannel: this.props.isPrivateChannel,
+    activeIndex: 0
+  };
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <div>MetaPanel</div>;
+    const { activeIndex, privateChannel, channel } = this.state;
+
+    if (privateChannel) return null;
+    return (
+      <Segment loading={!channel}>
+        <Header as="h3" attached="top">
+          About # {channel && channel.name}
+        </Header>
+        <Accordion styled attached="true">
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="info" />
+            Channel Details
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            {channel && channel.details}
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="user circle" />
+            Top Posters
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            Comming Soon...
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="pencil alternate" />
+            Created By
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            <Header as="h3">
+              <Image circular src={channel && channel.createdBy.avatar} />
+              {channel && channel.createdBy.name}
+            </Header>
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
